f6f4a609f3654a2dfaa21c10698b4239c4fcd302
Dexter Berend <dexterberend@gmail.com>
2021-12-25 23:31:41 -0600

    only reservations with a status of 'booked' can be edited p element

--- front-end/src/layout/reservations/edit/EditReservation.js
+++ front-end/src/layout/reservations/edit/EditReservation.js
@@ -1,8 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ErrorAlert from "../../ErrorAlert";
+import { getReservation } from "../../../utils/api";
+import { useParams } from "react-router-dom";
 
-function EditReservation({ reservation }) {
+function EditReservation() {
+
+    const [editingError, setEditingError] = useState(null);
+    const [reservation, setReservation] = useState({});
+
+    const { reservation_id } = useParams();
+
+    let statusMessage = reservation.status === 'booked' ? 
+        null :
+        <p>Only reservations with a status of 'booked' can be edited.</p>;
+
+    const loadReservation = () => {
+        const abortController = new AbortController();
+        getReservation(reservation_id)
+            .then((response) => {
+                setReservation(response.data);
+            })
+            .catch(setEditingError);
+        return () => abortController.abort();
+    }
+
+    useEffect(loadReservation, [reservation_id]);
+
     return (
         <>
+            <ErrorAlert error={editingError} />
+            {statusMessage}
         </>
     );
 }
